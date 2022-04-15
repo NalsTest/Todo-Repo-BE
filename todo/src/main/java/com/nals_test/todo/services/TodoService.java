8a0c186a81fe48cbca3b858140d38dccfce8e70a
BOC1096 <linh000boc@gmail.com>
2022-04-16 06:06:15 +0700

    create: create service
    add class TodoServiceImpl and interfaceTodoService

---
+++ todo/src/main/java/com/nals_test/todo/services/TodoService.java
@@ -0,0 +1,4 @@
+package com.nals_test.todo.services;
+
+public interface TodoService {
+}
---
+++ todo/src/main/java/com/nals_test/todo/services/TodoServiceImpl.java
@@ -0,0 +1,4 @@
+package com.nals_test.todo.services;
+
+public class TodoServiceImpl implements TodoService{
+}
