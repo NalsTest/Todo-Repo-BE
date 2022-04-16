282595e92023feabd2793d94d646fd724c7af84c
BOC1096 <linh000boc@gmail.com>
2022-04-16 17:13:31 +0700

    feat: add feature add new todo

--- todo/src/main/java/com/nals_test/todo/services/TodoService.java
+++ todo/src/main/java/com/nals_test/todo/services/TodoService.java
@@ -1,4 +1,16 @@
 package com.nals_test.todo.services;
 
+
+import com.nals_test.todo.model.entity.Todo;
+
+import java.util.List;
+
 public interface TodoService {
+    void add(Todo todo);
+
+    void edit(Todo todo);
+
+    List<Todo> findAll();
+
+    void delete(Integer id);
 }
--- todo/src/main/java/com/nals_test/todo/services/TodoServiceImpl.java
+++ todo/src/main/java/com/nals_test/todo/services/TodoServiceImpl.java
@@ -1,4 +1,37 @@
 package com.nals_test.todo.services;
 
-public class TodoServiceImpl implements TodoService{
+import com.nals_test.todo.model.entity.Todo;
+import com.nals_test.todo.repositories.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoServiceImpl implements TodoService {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Override
+    public void add(Todo todo) {
+        if (todo != null) {
+            todoRepository.save(todo);
+        }
+    }
+
+    @Override
+    public void edit(Todo todo) {
+
+    }
+
+    @Override
+    public List<Todo> findAll() {
+        return null;
+    }
+
+    @Override
+    public void delete(Integer id) {
+
+    }
 }
